cfecd6b735b2385eb2fade63dd697a3d1b3b9ebc
DevChris <chirsbalc27@gmail.com>
2023-11-08 13:30:06 -0400

    Line Up Section & Image Optims Complete
    Completed Line Up feature for the festival site. Added Gulp tasks for image minification (PNG/JPG) and new tasks for AVIF/WebP conversion to enhance load times and browser support.

--- festival-musica-project/gulpfile.js
+++ festival-musica-project/gulpfile.js
@@ -1,12 +1,21 @@
+//++++++++++Css++++++++++//
 import * as sass from 'sass';
-import gulpsass from 'gulp-sass';
-import pkg from 'gulp';
+import gulpSass from 'gulp-sass';
+
+//++++++++++Gulp++++++++++//
+import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 
-const { src, dest, watch } = pkg;
-const dartSass = gulpsass(sass);
+//++++++++++Images++++++++++//
+import webp from 'gulp-webp';
+import cache from 'gulp-cache';
+import imagemin from 'gulp-imagemin';
+import avif from 'gulp-avif'
 
-export function css(done) {
+const dartSass = gulpSass(sass);
+const { src, dest, watch, parallel } = gulp;
+
+function css(done) {
     src('./src/sass/**/*.scss')
         .pipe(plumber())
         .pipe(dartSass())
@@ -15,7 +24,40 @@ export function css(done) {
     done();
 }
 
-export function dev(done) {
+function lightImages(done) {
+    const options = {
+        optimizationLevel: 3,
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(options)))
+        .pipe(dest('build/img'));
+    done();
+}
+
+function versionWebp(done) {
+    const options = {
+        quality: 50,
+    };
+
+    src('src/img/**/*.{png,jpg}').pipe(webp(options)).pipe(dest('build/img'));
+
+    done();
+}
+
+function versionAvif(done) {
+    const options = {
+        quality: 50,
+    };
+
+    src('src/img/**/*.{png,jpg}').pipe(avif(options)).pipe(dest('build/img'));
+
+    done();
+}
+
+function watcher(done) {
     watch('./src/sass/**/*.scss', css);
     done();
 }
+
+export const dev = parallel(lightImages, versionWebp, versionAvif, watcher);
